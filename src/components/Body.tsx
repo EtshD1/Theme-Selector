dcf2ab68054be1ae3a5488e77b5ddaf4affab27b
EtshD1 <H79Pain@Gmail.com>
2021-04-27 23:53:10 +0200

    add about section

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -20,10 +20,14 @@ const Body = ({ context }: providedContext) => {
       <p>My stack includes: Node.js, Python, TypeScript, React, ExpressJS, Flask, Flutter, and Django</p>
     </div>
     <div ref={about}>
-      <h3>Home</h3>
-      <h4>Hello!</h4>
-      <p>I am a self-taught Web Developer who is always motivated to develop creative applications and improve upon myself. I always seek ways to do things differently, better, and faster.</p>
-      <p>My stack includes: Node.js, Python, TypeScript, React, ExpressJS, Flask, Flutter, and Django</p>
+      <h3>About</h3>
+      <h4>Motivation</h4>
+      <p>My motivation behind this project was to use TypeScript along with React.</p>
+      <p>Here are some links to resources that inspired me to do this project:</p>
+      <ul className={Styles.links}>
+        <li><a target="_blank" rel="noreferrer" href="https://youtu.be/ydkQlJhodio">How to use TypeScript with React... But should you?</a></li>
+        <li><a target="_blank" rel="noreferrer" href="https://youtu.be/rXuHGLzSmSE">CSS Theme Switcher by Reverse-Engineering Alligator.io</a></li>
+      </ul>
     </div>
   </div>);
 }
