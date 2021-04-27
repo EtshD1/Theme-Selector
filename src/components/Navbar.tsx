950588ca084e5f60c0339d3f51130181b6ffdff9
EtshD1 <H79Pain@Gmail.com>
2021-04-27 03:08:34 +0200

    add change theme functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,19 @@
 import React, { useRef } from 'react';
 import Styles from './styles/navbar.module.css';
+import withContext from '../withContext';
+import { ContextProviding } from "../Interfaces";
 
 interface link {
   name: string,
   to?: Function,
   href?: string
+}
+
+interface ThemeSelector {
+  dark: boolean,
+  solarized: boolean,
+  setDark: Function,
+  setSolarized: Function
 }
 
 const NavLink = (props: link) => <li>
@@ -13,8 +22,28 @@ const NavLink = (props: link) => <li>
   </div>
 </li>
 
-const DropDown = () => {
+const DropDown = ({ dark, solarized, setDark, setSolarized }: ThemeSelector) => {
   const Li = useRef<HTMLLIElement | null>(null);
+
+  const handleLight = () => {
+    if (dark) {
+      setDark(false);
+    }
+  }
+
+  const handleSolar = () => {
+    if (solarized) {
+      setSolarized(false);
+    } else {
+      setSolarized(true);
+    }
+  }
+
+  const handleDark = () => {
+    if (!dark) {
+      setDark(true);
+    }
+  }
 
   return <li ref={Li} className={Styles.themeSelector}>
     <div>
@@ -24,27 +53,29 @@ const DropDown = () => {
     </div>
     <ul className={Styles.dropdown}>
       <li>
-        <button>Light</button>
+        <button onClick={handleLight}>Light</button>
       </li>
       <li>
-        <button>Dark</button>
+        <button onClick={handleDark}>Dark</button>
       </li>
       <li>
-        <button>Solarized</button>
+        <button onClick={handleSolar}>Solarized</button>
       </li>
     </ul>
   </li>
 }
 
-const Navbar = () => {
-  return <nav>
+const Navbar = (props: ContextProviding) => {
+  const { dark, solarized, setDark, setSolarized } = props.context
+
+  return <nav className={[dark ? "dark" : "light", solarized ? "solar" : ""].join(" ")}>
     <ul className={Styles.navbar}>
       <NavLink name='Home' />
       <NavLink name='About' />
-      <DropDown />
+      <DropDown dark={dark} solarized={solarized} setDark={setDark} setSolarized={setSolarized} />
       <NavLink name='More' />
     </ul>
   </nav>
 }
 
-export default Navbar;
+export default withContext(Navbar);
