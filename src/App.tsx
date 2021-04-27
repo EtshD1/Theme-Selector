0ea6510350ec2310aa4d03ad86ef43735e42ca5c
EtshD1 <H79Pain@Gmail.com>
2021-04-27 03:07:54 +0200

    add context to children

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
+import "./components/styles/themes.css";
+import Context from './context';
 
 const App = () => {
-  return (<>
+  const [dark, setDark] = useState(false);
+  const [solarized, setSolarized] = useState(false);
+
+  return (<Context.Provider value={{ setSolarized, setDark, solarized, dark }}>
     <Navbar />
     <Title />
-  </>);
+  </Context.Provider>);
 }
 
 export default App;
