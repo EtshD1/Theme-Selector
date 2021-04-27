c43a14e8841cd28d11375e575fde1586163e169b
EtshD1 <H79Pain@Gmail.com>
2021-04-28 00:02:26 +0200

    change themeselector to hover instead of click add scroll function

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import Styles from './styles/navbar.module.css';
 import DropDown from './DropDown';
+import withContext from "../withContext";
+
+interface ContextProvided {
+  context: {
+    scrollIntoHome: Function,
+    scrollIntoAbout: Function,
+    scrollIntoMore: Function
+  }
+}
 
 interface link {
   name: string,
-  to?: Function,
-  href?: string
+  to: Function
 }
 
-const NavLink = (props: link) => <li>
-  <div>
-    <a href={props.href}>{props.name}</a>
-  </div>
-</li>
+const NavLink = (props: link) => {
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+    props.to();
+  }
 
-const Navbar = () => {
+  return <li>
+    <div>
+      <a href="/" onClick={handleClick}>{props.name}</a>
+    </div>
+  </li>
+}
+
+const Navbar = ({ context }: ContextProvided) => {
+  const { scrollIntoAbout, scrollIntoHome, scrollIntoMore } = context;
   return <nav>
     <ul className={Styles.navbar}>
-      <NavLink name='Home' />
-      <NavLink name='About' />
+      <NavLink name='Home' to={scrollIntoHome} />
+      <NavLink name='About' to={scrollIntoAbout} />
       <DropDown />
-      <NavLink name='More' />
+      <NavLink name='More' to={scrollIntoMore} />
     </ul>
   </nav>
 }
 
-export default Navbar;
+export default withContext(Navbar);
